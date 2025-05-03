240293832231d2447808f3eb44fb59a6d48515e8
heega <jsa7868@gmail.com>
2025-05-03 20:31:01 +0900

    ch01

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,13 @@
+package sec03.exam01;
+/*
+ 	작성자 : 남가희
+ 	작성일 : 2025-05-03
+ */
+
+public class Hello {
+	// 프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 콘솔에 출력한ㄴ 실행문
+		System.out.println("Hello, Java");
+	} // end of main
+} //end of class
