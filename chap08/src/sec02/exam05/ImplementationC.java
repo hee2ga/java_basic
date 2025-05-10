4bc09998e33d0e9c64b95035210d297e784becfc
heega <jsa7868@gmail.com>
2025-05-10 23:39:03 +0900

    ch08

---
+++ chap08/src/sec02/exam05/ImplementationC.java
@@ -0,0 +1,21 @@
+package sec02.exam05;
+
+public class ImplementationC implements InterfaceC {
+
+	@Override
+	public void methodA() {
+		System.out.println("Implementation-methodA() 실행");
+	}
+
+	@Override
+	public void methodB() {
+		System.out.println("Implementation-methodB() 실행");
+	}
+
+	@Override
+	public void methodC() {
+		System.out.println("Implementation-methodC() 실행");
+	}
+	
+
+}
